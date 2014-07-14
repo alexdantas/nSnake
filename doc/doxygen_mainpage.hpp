3059577992f371e2b1a676ad90761d7ef3177071
Alexandre Dantas <eu@alexdantas.net>
2014-07-14 16:33:19 -0300

    Updated `Doxyfile` to Doxygen version 1.8.7 and included a main page for the documentation

---
+++ doc/doxygen_mainpage.hpp
@@ -0,0 +1,21 @@
+///	@mainpage Documentation Main Page
+///
+/// _Welcome to the nSnake code documentation!_ <br /><br />
+///
+/// This page is for people who want to understand how nSnake was made
+/// (it's source code). <br />
+/// If you're not after this, check out the links below:
+///
+/// * [nSnake homepage](http://nsnake.alexdantas.net)
+/// * [nSnake on GitHub](https://github.com/alexdantas/nsnake)
+///
+/// \tableofcontents
+///
+///  ## Introduction
+///
+///  ...<b>I really should put something here</b>...
+///
+///  ## Contact
+///
+///  Any questions, please email me at <eu@alexdantas.net>
+
